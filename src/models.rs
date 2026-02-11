e6bcf86ac208f79d25934d53e567379eac36622a
terry-c-8512352 <261058758+terry-c-8512352@users.noreply.github.com>
2026-02-11 22:56:40 +0000

    basic structure

---
+++ src/models.rs
@@ -0,0 +1,9 @@
+pub struct ModDiscriptor {
+    pub tags: Vec<String>,
+    pub name: String,
+    pub path: String,
+    pub supported_version: String,
+    pub remote_file_id: String,
+    pub picture: Option<String>,
+    pub version: Option<String>,
+}
